443b43edb97c4a48dea0ae651d2df9bc2e41bcec
MichalGomulczak <michal.gomulczak1@gmail.com>
2025-04-29 14:59:48 +0200

    Make engine a class

--- src/engine.py
+++ src/engine.py
@@ -1,5 +1,21 @@
 import demucs
 
-def processSong(songPath : str) -> int:
-  raise NotImplementedError
+class Engine:
+    def __init__(self, songPath: str):
+        """
+        Starts processing song given by local songPath
+        """
+        self.process_song()
+        raise NotImplementedError()
 
+    def is_ready(self) -> bool:
+        """
+        Whether song is ready to be sent do client
+        """
+        raise NotImplementedError()
+
+    def process_song(self) -> int:
+        """
+        Processes song, returns status code
+        """
+        raise NotImplementedError()
