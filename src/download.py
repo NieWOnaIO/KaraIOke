e4a3ccfd958f4a2122afdafce84122450bedcf40
Rellikeht <michaelhemperek@gmail.com>
2025-05-04 14:10:35 +0200

    add downloader

--- src/download.py
+++ src/download.py
@@ -1,28 +1,78 @@
 import yt_dlp  # type: ignore
-import hashlib
+from hashlib import sha256
+from concurrent import futures
+from os import makedirs
+from multiprocessing import cpu_count
+import json
 
 
 class Download:
+    __threads = cpu_count() * 2 if cpu_count() <= 2 else cpu_count() + 1
+    __executor = futures.ThreadPoolExecutor(__threads)
+
+    @staticmethod
+    def parse_info(info: dict, link: str) -> dict:
+        return {
+            "link": link,
+            "title": info.get("track") or info.get("title"),
+            "author": info.get("artist") or info.get("uploader"),
+            "album": info.get("album") or "Unknown",
+            "release": info.get("release_year")
+            or info.get("upload_date")[:4],
+            "duration": info.get("duration"),
+        }
+
     def __init__(self, link: str):
         """
         Starts downloading video given by link and stores
         name of folder with results internally
         """
         self.link = link
-        self.__name = None  # place future here
-        raise NotImplementedError()
+        self.__name = sha256(link.encode()).hexdigest()
+        song_dir = f"downloads/{self.__name}"
+        makedirs(song_dir, exist_ok=True)
+        song_file = f"{song_dir}/raw_song.%(ext)s"
+        metadata_file = f"{song_dir}/metadata.json"
+
+        ytdl_opts = {
+            "format": "bestaudio/best",
+            "extract-audio": True,
+            "outtmpl": song_file,
+            "no-post-overwrites": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "112k",
+                }
+            ],
+            "quiet": True,
+        }
+
+        with yt_dlp.YoutubeDL(ytdl_opts) as ytdl:
+
+            def helper():
+                info = ytdl.extract_info(link, download=False)
+                data = Download.parse_info(info, link)
+                with open(metadata_file, "w") as output:
+                    json.dump(data, output, indent=4)
+
+            self.__downloader = self.__executor.submit(ytdl.download, [link])
+            self.__informator = self.__executor.submit(helper)
 
     def is_ready(self) -> bool:
         """
-        Returns readiness of download
+        Tells if download is ready
         """
-        # TODO some future status
-        raise NotImplementedError()
+        return self.__downloader.done() and self.__informator.done()
 
     def get_name(self) -> str:
         """
         Awaits for end of downloading and returns directory name
         when it is finished
         """
-        # TODO await for future here
-        raise NotImplementedError()
+        self.__informator.result()
+        self.__downloader.result()
+        if self.__name == "":
+            raise Exception("Something gone wrong with your download")
+        return self.__name
