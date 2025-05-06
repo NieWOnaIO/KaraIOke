d1d629ee4870325e2e0470613ed1feb5f43d1d57
mpus <mateuszpodmokly@outlook.com>
2025-05-06 23:29:26 +0200

    error handling

--- src/api_routes.py
+++ src/api_routes.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi import HTTPException
 import json
+import asyncio
+import os
+
 from engine import Engine
 from search import Search
 from download import Download
@@ -17,10 +21,38 @@ async def process_song(link):
     Returns:
         json: unique song id based on youtube url
     """
+    timeout_download = 60
     download = Download(link)
-    song_id = download.get_name()
-    engine.enqueue(f"downloads/{song_id}/audio.mp3")
-    return {"song_id": song_id}
+
+    success = False
+    for _ in range(timeout_download):
+        if download.is_ready():
+            success = True
+            break
+        await asyncio.sleep(1)
+
+    if not success:
+        return {"status": "download timeout", "song_id": ""}
+    
+    try:
+        song_id = download.get_name()
+        engine.enqueue(f"downloads/{song_id}/audio.mp3")
+    except Exception as e:
+        return {"status": e, "song_id": ""}
+    
+    timeout_process = 180
+    success = False
+
+    for _ in range(timeout_process):
+        if download.is_ready():
+            success = True
+            break
+        await asyncio.sleep(1)
+    
+    if not success:
+        return {"status": "processing timeout", "song_id": ""}
+    
+    return {"status": "success", "song_id": song_id}
 
 @app.get("/v1/songinfo/{song_id}")
 async def get_songinfo(song_id: str):
@@ -28,8 +60,11 @@ async def get_songinfo(song_id: str):
     Returns:
         json: whether song is ready to be downloaded from the server, expiry date...
     """
-    songinfo = json.load(f"downloads/{song_id}/metadata.json")
-    return songinfo
+    path = f"downloads/{song_id}/metadata.json"
+    if not os.path.exists(path):
+        raise HTTPException(status_code=404, detail="Path does not exist")
+
+    return json.load(path)
 
 @app.get("/v1/songs/{song_id}")
 async def get_songfile(song_id: str):
@@ -37,10 +72,14 @@ async def get_songfile(song_id: str):
     Returns:
         payload: processed song along with metadata
     """
+    path = f"downloads/{song_id}/audio.mp3"
+    if not os.path.exists(path):
+        raise HTTPException(status_code=404, detail="Path does not exist")
+    
     return FileResponse(
-        path=f"downloads/{song_id}/raw_song.mp3",
+        path=f"downloads/{song_id}/audio.mp3",
         media_type="audio/mpeg",
-        filename="raw_song.mp3"
+        filename="audio.mp3"
     )
 
 @app.get("/v1/search/{query}")
@@ -50,5 +89,5 @@ async def search_song(query: str):
         json: Search results from a query
     """
     search = Search(query)
-    search.search_query()
+    # errors checking
     return search.serialize()
