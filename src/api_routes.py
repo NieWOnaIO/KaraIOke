dfd7da7d7f5b1a0f7f0a15ceab6805e002f699d5
mpus <mateuszpodmokly@outlook.com>
2025-05-08 17:35:40 +0200

    fixed returning files & status system

--- src/api_routes.py
+++ src/api_routes.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi import HTTPException
 import json
-import asyncio
+import zipfile
 import os
 
 from engine import Engine
@@ -21,39 +21,16 @@ async def process_song(link):
     Returns:
         json: unique song id based on youtube url
     """
-    timeout_download = 60
     download = Download(link)
-
-    success = False
-    for _ in range(timeout_download):
-        if download.is_ready():
-            success = True
-            break
-        await asyncio.sleep(1)
-
-    if not success:
-        return HTTPException(status_code=504, detail="Downloading timeout")
     
     try:
-        song_id = download.get_name()
+        song_id = await download.get_name()
     except Exception as e:
         return HTTPException(status_code=502, detail=e)
     try:
         engine.enqueue(f"downloads/{song_id}")
     except Exception as e:
         return HTTPException(status_code=400, detail=e)
-    
-    timeout_process = 180
-    success = False
-
-    for _ in range(timeout_process):
-        if engine.is_done(f"downloads/{song_id}"):
-            success = True
-            break
-        await asyncio.sleep(1)
-    
-    if not success:
-        return HTTPException(status_code=504, detail="Processing timeout")
     
     return {"song_id": song_id}
 
@@ -67,7 +44,7 @@ async def get_songinfo(song_id: str):
     if not os.path.exists(path):
         raise HTTPException(status_code=404, detail="Path does not exist")
 
-    if engine.is_done(path):
+    if not engine.is_done(path):
         return {"status": "not_ready"}
     
     file = open(path, "r")
@@ -81,16 +58,23 @@ async def get_songfile(song_id: str):
     Returns:
         payload: processed song along with metadata
     """
-    base_path = f"downloads/{song_id}/htdemucs/audio"
-    if not os.path.exists(f"{base_path}/vocals.mp3") or\
-        not os.path.exists(f"{base_path}/no_vocals.mp3"):
+    base_path = os.path.join("downloads", song_id, "htdemucs", "audio")
+    if not os.path.exists(os.path.join(base_path, "vocals.mp3")) or\
+        not os.path.exists(os.path.join(base_path, "no_vocals.mp3")):
         raise HTTPException(status_code=404, detail="Path does not exist")
     
-    return {
-        "vocals": f"{base_path}/vocals.mp3",
-        "no_vocals": f"{base_path}/no_vocals.mp3",
-        "metadata": f"downloads/{song_id}/metadata.json"
-    }
+    zip_path = f"downloads/{song_id}"
+    zipf = zipfile.ZipFile(os.path.join(zip_path, "package.zip"), "w")
+    zipf.write(os.path.join(base_path, "vocals.mp3"), arcname="vocals.mp3")
+    zipf.write(os.path.join(base_path, "no_vocals.mp3"), arcname="no_vocals.mp3")
+    zipf.write(os.path.join(zip_path, "metadata.json"), arcname="metadata.json")
+    zipf.close()
+
+    return FileResponse(
+        path=os.path.join(zip_path, "package.zip"),
+        filename="package.zip",
+        media_type="application/zip"
+    )
 
 @app.get("/v1/search/{query}")
 async def search_song(query: str):
