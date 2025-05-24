e211b3d4f6a2997cc95892b182f0795be478929b
Rellikeht <michaelhemperek@gmail.com>
2025-05-24 12:22:00 +0200

    update tests to work with new version of code

--- tests/backend_tests.py
+++ tests/backend_tests.py
@@ -9,10 +9,12 @@ from os.path import abspath
 from pathlib import Path
 
 path.insert(0, abspath(Path(argv[0]) / "../.."))
+path.insert(0, abspath(Path(argv[0]) / "../../src"))
 
 from src.search import Search
 from src.download import Download
 from src.engine import Engine
+
 
 class TestSearch(unittest.TestCase):
     def test_song_search(self):
@@ -23,13 +25,24 @@ class TestSearch(unittest.TestCase):
         result = Search(query).results
         s1, s2 = query.split(" ")[:2]
 
-        self.assertTrue(re.search(rf'\b{s1}\b', result[0]["title"], re.IGNORECASE))
-        self.assertTrue(re.search(rf'\b{s2}\b', result[0]["title"], re.IGNORECASE))
+        self.assertTrue(
+            re.search(rf"\b{s1}\b", result[0]["title"], re.IGNORECASE)
+        )
+        self.assertTrue(
+            re.search(rf"\b{s2}\b", result[0]["title"], re.IGNORECASE)
+        )
         self.assertEqual(len(result), 20)
 
         for res in result:
-            self.assertTrue(bool(re.match(r"(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)[\w\-]{11}", res["url"])))
-        
+            self.assertTrue(
+                bool(
+                    re.match(
+                        r"(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)[\w\-]{11}",
+                        res["url"],
+                    )
+                )
+            )
+
 
 class TestDownload(unittest.TestCase):
     def test_song_download(self):
@@ -40,6 +53,7 @@ class TestDownload(unittest.TestCase):
         result = Search(query).results[0]["url"]
         dl = Download(result)
         dir = dl.get_name()
+        dl.wait_for()
 
         path = os.path.join("downloads", dir)
         self.assertTrue(os.path.isdir(path))
@@ -52,7 +66,8 @@ class TestDownload(unittest.TestCase):
 
         self.assertGreaterEqual(os.path.getsize(audio_path), 128)
         self.assertGreaterEqual(os.path.getsize(meta_path), 32)
-        
+
+
 class TestEngine(unittest.TestCase):
     def test_song_processing(self):
         """
@@ -66,7 +81,7 @@ class TestEngine(unittest.TestCase):
             if e.is_done(path):
                 break
             time.sleep(1)
-        
+
         path = os.path.join(path, "htdemucs", "audio")
         self.assertTrue(os.path.isdir(path))
 
@@ -75,12 +90,12 @@ class TestEngine(unittest.TestCase):
 
         no_vocals_path = os.path.join(path, "no_vocals.mp3")
         self.assertTrue(os.path.isfile(no_vocals_path))
-        
+
         self.assertGreaterEqual(os.path.getsize(vocals_path), 128)
         self.assertGreaterEqual(os.path.getsize(no_vocals_path), 128)
-        
+
         shutil.rmtree(os.path.abspath("downloads"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-    
